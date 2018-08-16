3ee24ce3dcad3b27111eafe5f9f65fd874ed5957
Sarah Yan <Sarah@s-8d3a346b.on.site.uni-stuttgart.de>
2018-08-16 16:25:09 +0200

    more stuff

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
 import HomePage from './HomePage';
-import NavBar from './HeaderComponent/NavBar';
+import NavBar from './NavBar';
 class App extends Component {
   render() {
     return (
