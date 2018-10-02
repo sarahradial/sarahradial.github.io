cf927f5c7d9d371ff273ff68bca9572237f94e89
Sarah Yan <syan@wellesley.edu>
2018-10-01 23:41:52 -0400

    doneeeee

--- hwk2/clown.js
+++ hwk2/clown.js
@@ -4,9 +4,9 @@ function createClownParams(){
         // clown component variables
         originRadius: 0.25,
         headRadius: 5,
-        hatRadius: 6, hatBaseHeight: 0.25, hatHeight: 7, hatTopRadius: 5.5, hatBottomRadius: 5, hatAngle: Math.PI*1/3,
-        eyeRadius: 1, noseRadius: 0.5, earRadius: 2, eyeSpace: 4, eyeHeight: 1,
-        mouthRadius: 2, mouthTube: 0.25, mouthHeight: -2, mouthRotation: Math.PI*1/4, mouthArc: Math.PI*1/2,
+        hatRadius: 5, hatDistanceX: 1.5, hatBaseHeight: 0.5, hatHeight: 7, hatTopRadius: 5, hatBottomRadius: 3, hatAngle: -Math.PI*1/12,
+        eyeRadius: 0.75, noseRadius: 0.5, earRadius: 2, eyeSpace: 4, eyeHeight: 1,
+        mouthRadius: 2, mouthTube: 0.25, mouthHeight: -0.5, mouthRotation: -Math.PI*2/3, mouthArc: Math.PI*1/2,
         bodyRadius: 6, bodyScaleY: 1.2,
         armTopRadius: 0.75, armBottomRadius: 0.5, armLength: 8, armAngle: Math.PI*1/4,
         shoulderHeight: 9, shoulderWidth: 7, shoulderRadius: 2,
@@ -95,18 +95,14 @@ function createArm(params, side){
     var hand = createJoint(handRadius, segments2, handMaterial);
 
     // position shoulder and hand
-    shoulder.position.y = armTopRadius;
-    shoulder.position.x = 0;
-    hand.position.y = armBottomRadius;
-    hand.position.x = -armLength;
+    shoulder.position.set(0, armLength/2 - shoulderRadius/2, 0);
+    hand.position.set(0, -armLength/2 + handRadius/2, 0);
     arm.add(shoulder);
     arm.add(hand);
 
     // position arm
+    arm.position.set(side*(params.shoulderWidth/2+armLength/2), params.bodyRadius*params.bodyScaleY+params.shoulderHeight, 0);
     arm.rotation.z = side*params.armAngle;
-    arm.position.y = params.legLength+params.shoulderHeight;
-    arm.position.x = side*(params.shoulderWidth/2);
-
     // right when side == 1, left when side == -1
     arm.name = (side == 1 ? "right arm": "left arm");
     return arm;
@@ -122,7 +118,7 @@ function createBody(bodyRadius, segments, material, bodyScaleY){
     return body;
 }
 // creates hat
-function createHat(hatRadius, hatBaseHeight, hatHeight, hatTopRadius, hatBottomRadius, material, segments){
+function createHat(hatDistanceX, hatRadius, hatBaseHeight, hatHeight, hatTopRadius, hatBottomRadius, hatAngle, material, segments){
     var hat = new THREE.Object3D();
     var hatGeom = new THREE.CylinderGeometry(hatTopRadius, hatBottomRadius, hatHeight, segments, segments);
     var hatMesh = new THREE.Mesh(hatGeom, material);
@@ -130,7 +126,9 @@ function createHat(hatRadius, hatBaseHeight, hatHeight, hatTopRadius, hatBottomR
     hat.add(hatMesh);
     var baseHatGeom = new THREE.CylinderGeometry(hatRadius, hatRadius, hatBaseHeight, segments, segments);
     var hatBaseMesh = new THREE.Mesh(baseHatGeom, material);
+    hatBaseMesh.position.set(0, -hatHeight/2, 0);
     hat.add(hatBaseMesh);
+    hat.position.set(hatDistanceX, hatHeight + hatBaseHeight, 0);
     return hat;
 }
 // creates and positions foot while adding to clown
@@ -164,7 +162,6 @@ function addLeg(clown, params, side){
 function addArms(clown, params){
     var leftArm = createArm(params, -1);
     var rightArm = createArm(params, 1);
-
     clown.add(leftArm);
     clown.add(rightArm);
 }
@@ -191,12 +188,13 @@ function addFace(head, params){
     var leftEar = createJoint(params.earRadius, params.SphereGeometry, params.faceMaterial);
     var mouth = createMouth(params.mouthRadius, params.mouthTube, params.sphereDetail, params.mouthArc, params.mouthMaterial);
     // position them relative to head
-    rightEye.set.position(1*params.eyeSpace/2, params.eyeHeight, params.headRadius);
-    leftEye.set.position(-1*params.eyeSpace/2, params.eyeHeight, params.headRadius);
-    nose.set.position(0, 0, params.headRadius);
-    rightEar.set.position(1*params.headRadius, params.headRadius, 0);
-    leftEar.set.position(-1*params.headRadius, params.headRadius, 0);
-    mouth.set.position(0, params.mouthHeight, 0);
+    rightEye.position.set(1*params.eyeSpace/2, params.eyeHeight, params.headRadius);
+    leftEye.position.set(-1*params.eyeSpace/2, params.eyeHeight, params.headRadius);
+    nose.position.set(0, 0, params.headRadius);
+    rightEar.position.set(1*params.headRadius, 0, 0);
+    leftEar.position.set(-1*params.headRadius, 0, 0);
+    mouth.rotation.z = params.mouthRotation;
+    mouth.position.set(0, params.mouthHeight, params.headRadius);
     // add them to head
     head.add(rightEye);
     head.add(leftEye);
@@ -213,14 +211,13 @@ function addHead(clown, params){
     var head = createJoint(headRadius, segments, headMaterial);
     head.name = "head";
     // add hat
-    var hat = createHat(params.hatRadius, params.hatBaseHeight, params.hatHeight, params.hatTopRadius, params.hatBottomRadius, params.hatMaterial, params.cylinderDetail);
-    hat.set.position(0, 2*headRadius, 0);
-    hat.rotation.z = params.hatAngle;
+    var hat = createHat(params.hatDistanceX, params.hatRadius, params.hatBaseHeight, params.hatHeight, params.hatTopRadius, params.hatBottomRadius, params.hatAngle, params.hatMaterial, params.cylinderDetail);
     head.add(hat);
+    hat.rotation.z = params.hatAngle;
     // add facial features to face
     addFace(head, params);
     // set position of entire head + components
-    var headPositiony = params.legLength + 2*params.bodyRadius*params.bodyScaleY+headRadius;
+    var headPositiony = params.legLength + 2*params.bodyRadius*params.bodyScaleY+headRadius/2;
     head.position.set(0, headPositiony, 0);
     clown.add(head);
 }
