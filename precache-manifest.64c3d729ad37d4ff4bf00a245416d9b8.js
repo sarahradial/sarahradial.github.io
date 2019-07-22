625bafb72c26ae1ea5804ae1797c8d72504e77b0
Sarah Yan <syan@wellesley.edu>
2019-07-22 19:02:47 -0400

    Updates

---
+++ precache-manifest.64c3d729ad37d4ff4bf00a245416d9b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ea254355ad7f67c700a9b6b7df3e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24febe25bb314d914126",
+    "url": "/static/css/main.f63912e2.chunk.css"
+  },
+  {
+    "revision": "65d7d281f63ee79bb2fb",
+    "url": "/static/js/2.392ad67f.chunk.js"
+  },
+  {
+    "revision": "24febe25bb314d914126",
+    "url": "/static/js/main.ec864900.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
